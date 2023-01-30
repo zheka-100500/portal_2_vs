dfc3fa7848164b4624b8effc444645dee3e29cca
zheka-100500 <zheka_100500@mail.ru>
2023-01-31 02:08:28 +0300

    Create extension.js

---
+++ src/extension.js
@@ -0,0 +1,16 @@
+const vscode = require('vscode');
+
+function activate(context)
+{
+    console.log('Hi!');
+}
+
+function deactivate()
+{
+
+}
+
+module.exports = {
+    activate,
+    deactivate
+}
